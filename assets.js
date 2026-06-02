8f5878fb7301d76da14680745c1376c2f3987bb4
oiyui2 <yhp6613@gmail.com>
2026-06-02 22:08:36 +0900

    Update assets.js

--- assets.js
+++ assets.js
@@ -1,1 +1,48 @@
+function preload() {
+// 0번 캐릭터: 불
+characters[0] = [
+loadImage("images/1단계불.png"),
+loadImage("images/2단계불.png"),
+loadImage("images/3단계불.png"),
+loadImage("images/4단계불.png"),
+loadImage("images/5단계불.png")
+];
 
+// 1번 캐릭터: 구름
+characters[1] = [
+loadImage("images/1단계구름.png"),
+loadImage("images/2단계구름.png"),
+loadImage("images/3단계구름.png"),
+loadImage("images/4단계구름.png"),
+loadImage("images/5단계구름.png")
+];
+
+// 2번 캐릭터: 유령
+characters[2] = [
+loadImage("images/1단계유령.png"),
+loadImage("images/2단계유령.png"),
+loadImage("images/3단계유령.png"),
+loadImage("images/4단계유령.png"),
+loadImage("images/5단계유령.png")
+];
+
+// 3번 캐릭터: 구
+characters[3] = [
+loadImage("images/1단계구.png"),
+loadImage("images/2단계구.png"),
+loadImage("images/3단계구.png"),
+loadImage("images/4단계구.png"),
+loadImage("images/5단계구.png")
+];
+
+songSounds[0] = loadSound("sounds/song1.mp3");
+// songSounds[1] = loadSound("sounds/song2.mp3");
+// songSounds[2] = loadSound("sounds/song3.mp3");
+// songSounds[3] = loadSound("sounds/song4.mp3");
+// songSounds[4] = loadSound("sounds/song5.mp3");
+// songSounds[5] = loadSound("sounds/song6.mp3");
+}
+
+// ============================================================
+// setup
+// ============================================================
